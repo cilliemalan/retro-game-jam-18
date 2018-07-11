266f2a62f3a160fff0810b0d7ae752088429c84d
Cillié Malan <cilliemalan@gmail.com>
2018-07-11 13:40:13 +0200

    Slgihtly more async loading

--- src/index.js
+++ src/index.js
@@ -1,31 +1,5 @@
-import "phaser";
 
-var config = {
-  type: Phaser.AUTO,
-  parent: "phaser-example",
-  width: 800,
-  height: 600,
-  scene: {
-    preload: preload,
-    create: create
-  }
-};
-
-const game = new Phaser.Game(config);
-
-function preload() {
-  this.load.image("logo", "assets/logo.png");
-}
-
-function create() {
-  var logo = this.add.image(400, 150, "logo");
-
-  this.tweens.add({
-    targets: logo,
-    y: 450,
-    duration: 2000,
-    ease: "Power2",
-    yoyo: true,
-    loop: -1
-  });
-}
+// load phaser async and then game
+import("phaser")
+  .then(() => import("./game"))
+  .then(() => document.getElementById("loadingmessage").remove());
