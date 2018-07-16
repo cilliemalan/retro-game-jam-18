5cc87bb95fb731347028441b2296f3d14c452679
Cillié Malan <cilliemalan@gmail.com>
2018-07-16 21:28:54 +0200

    Auto resize logic

--- src/game.js
+++ src/game.js
@@ -1,17 +1,31 @@
-import { Loader } from './scenes/loader';
-import { Main } from './scenes/main';
-import { Title } from './scenes/title';
+import { Loader } from "./scenes/loader";
+import { Main } from "./scenes/main";
+import { Title } from "./scenes/title";
+
+const zoom = 4;
 
 const config = {
-  type: Phaser.AUTO,
+  type: Phaser.WEBGL,
   parent: "gamecanvas",
-  width: 800,
-  height: 600,
-  scene: [
-    Loader,
-    Title,
-    Main
-  ]
+  backgroundColor: "#1b2632",
+  pixelArt: true,
+  zoom,
+  width: innerWidth / zoom,
+  height: innerHeight / zoom,
+
+  scene: [Loader, Title, Main]
 };
 
 const game = new Phaser.Game(config);
+window.game = game;
+window.addEventListener("resize", resize);
+resize();
+
+function resize() {
+  const { innerWidth: w, innerHeight: h } = window;
+  const { canvas } = game;
+
+  game.resize(w / zoom, h / zoom);
+  canvas.style.width = `${w}px`;
+  canvas.style.height = `${h}px`;
+}
